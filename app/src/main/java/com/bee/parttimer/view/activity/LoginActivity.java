b03050c0ad466f4196ee74d9ac1990f199dd52de
SUHONGJIN <903502825@qq.com>
2018-11-08 13:45:46 +0800

    添加Log日志测试一下。

--- app/src/main/java/com/bee/parttimer/view/activity/LoginActivity.java
+++ app/src/main/java/com/bee/parttimer/view/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.bee.parttimer.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.bee.parttimer.R;
 
@@ -11,5 +12,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i("tag","测试专用");
     }
 }
