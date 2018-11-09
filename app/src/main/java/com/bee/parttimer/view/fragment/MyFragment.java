16f5779a8dbb58ce323598ab3c13ad7929676f92
SUHONGJIN <903502825@qq.com>
2018-11-09 15:26:26 +0800

    添加头像背景模糊

--- app/src/main/java/com/bee/parttimer/view/fragment/MyFragment.java
+++ app/src/main/java/com/bee/parttimer/view/fragment/MyFragment.java
@@ -1,20 +1,35 @@
 package com.bee.parttimer.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.bee.parttimer.R;
+import com.bee.parttimer.view.activity.UserDataActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MyFragment extends Fragment {
+    @BindView(R.id.rl_userdata)
+    RelativeLayout rl_userdata;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_my,container,false);
+        ButterKnife.bind(this,view);
         return view;
+    }
+    @OnClick(R.id.rl_userdata)
+    public void click(View view){
+        startActivity(new Intent(getContext(), UserDataActivity.class));
     }
 }
