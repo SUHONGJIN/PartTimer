3cb17039812c8f53ed3b767e49f94bc09d4f3e22
SUHONGJIN <903502825@qq.com>
2018-11-10 08:52:39 +0800

    对话框

--- app/src/main/java/com/bee/parttimer/view/fragment/PostFragment.java
+++ app/src/main/java/com/bee/parttimer/view/fragment/PostFragment.java
@@ -1,11 +1,15 @@
 package com.bee.parttimer.view.fragment;
 
+import android.app.Dialog;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 
 import com.bee.parttimer.R;
 import com.bee.parttimer.base.BaseFragment;
 import com.bee.parttimer.utils.ToastUtils;
+import com.bee.parttimer.widget.WeiboDialogUtils;
 
 import butterknife.BindView;
 
@@ -13,6 +17,18 @@ public class PostFragment extends BaseFragment {
     @BindView(R.id.float_btn)
     FloatingActionButton float_btn;
 
+    private Dialog mWeiboDialog;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    WeiboDialogUtils.closeDialog(mWeiboDialog);
+                    break;
+            }
+        }
+    };
     @Override
     public int getLayoutResId() {
         return R.layout.fragment_post;
@@ -24,7 +40,8 @@ public class PostFragment extends BaseFragment {
         float_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ToastUtils.setOkToast(getContext(),"悬浮按钮！");
+                mWeiboDialog = WeiboDialogUtils.createLoadingDialog(getContext(), "加载中...");
+                mHandler.sendEmptyMessageDelayed(1, 3000);
             }
         });
     }
